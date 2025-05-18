259ddbeb1c1121befb895d9bfe76598036651c21
wolmanr <racheliwolman0462@gmail.com>
2025-05-18 13:52:50 -0400

    Recipe without related files

--- Database Files/Recipe Data Without Related Files.sql
+++ Database Files/Recipe Data Without Related Files.sql
@@ -24,3 +24,33 @@ create table dbo.Recipe(
                     else 'Draft' end persisted
 )
 go
+
+use recipedb
+go
+
+
+delete from Recipe;
+
+go
+
+
+insert  Cuisine(CuisineName)
+select 'american'
+union select 'french'
+union select 'english'
+union select 'italian';
+go
+
+insert  dbo.Recipe(CuisineId, UserId, RecipeName, Calories, CreatedDate, PublishedDate, ArchivedDate)
+select c.CuisineId, u.UserId, r.RecipeName, r.Calories, r.CreatedDate, r.PublishedDate, r.ArchivedDate
+from
+    (values 
+        ('chocolate chip cookies', 100, '2005-07-01', '2005-11-25', null, 'american', 'janesmith'),
+        ('apple yogurt smoothie', 50, '2019-04-18', '2019-07-21', '2023-09-21', 'french', 'janesmith'),
+        ('cheese bread', 130, '2021-07-14', '2021-08-12', null, 'english', 'frankslin'),
+        ('butter muffins', 60, '2022-11-27', '2023-01-14', '2024-01-01', 'american', 'larygrim'),
+        ('chocolate cake', 100, '2005-07-01', null, null, 'american', 'johndoe')
+    ) as r (RecipeName, Calories, CreatedDate, PublishedDate, ArchivedDate, CuisineName, UserName)
+join dbo.Cuisine c on c.CuisineName = r.CuisineName
+join dbo.Users u on u.UserName = r.UserName;
+go
