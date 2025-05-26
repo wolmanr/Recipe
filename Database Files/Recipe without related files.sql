a94eaee298557bd703d7467de07c5bcf894d3ea1
wolmanr <racheliwolman0462@gmail.com>
2025-05-26 15:20:19 -0400

    #60

---
+++ Database Files/Recipe without related files.sql
@@ -0,0 +1,10 @@
+insert  dbo.Recipe(CuisineId, UserId, RecipeName, Calories, CreatedDate, PublishedDate, ArchivedDate)
+select c.CuisineId, u.UserId, r.RecipeName, r.Calories, r.CreatedDate, r.PublishedDate, r.ArchivedDate
+from
+    (values 
+        ('chicken soup', 100, '2015-07-01', '2018-11-25', null, 'american', 'janesmith')  
+    ) as r (RecipeName, Calories, CreatedDate, PublishedDate, ArchivedDate, CuisineName, UserName)
+join dbo.Cuisine c on c.CuisineName = r.CuisineName
+join dbo.Users u on u.UserName = r.UserName;
+
+select * from Recipe
