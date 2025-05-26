ea02f635ab219a05bdfd47049ac4e4cf1a2c076a
wolmanr <racheliwolman0462@gmail.com>
2025-05-26 15:29:13 -0400

    #1

--- Database Files/Recipe without related files.sql
+++ Database Files/Recipe without related files.sql
@@ -2,8 +2,9 @@ insert  dbo.Recipe(CuisineId, UserId, RecipeName, Calories, CreatedDate, Publish
 select c.CuisineId, u.UserId, r.RecipeName, r.Calories, r.CreatedDate, r.PublishedDate, r.ArchivedDate
 from
     (values 
-        ('chicken soup', 100, '2015-07-01', '2018-11-25', null, 'american', 'janesmith')  
+        ('chicken ', 100, '2015-07-01', '2018-11-25', null, 'american', 'janesmith')  
     ) as r (RecipeName, Calories, CreatedDate, PublishedDate, ArchivedDate, CuisineName, UserName)
 join dbo.Cuisine c on c.CuisineName = r.CuisineName
 join dbo.Users u on u.UserName = r.UserName;
 
+select * from recipe
